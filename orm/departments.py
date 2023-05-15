1a3d9cb38d59ca22d80883769d8d3bc28aaf3f3d
JorgeAque <48557503+AqueJorge@users.noreply.github.com>
2023-05-15 15:41:04 -0600

    Add files via upload

---
+++ orm/departments.py
@@ -0,0 +1,14 @@
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+class Department(Base):
+    __tablename__ = 'departments'
+
+    id = Column(Integer, primary_key=True)
+    department = Column(String(255), nullable=False)
+
+def process_department(id, department):
+    department_obj = Department(id=id, department=department)
+    return department_obj
